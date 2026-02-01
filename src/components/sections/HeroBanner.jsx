e1740f0cd9844bb3851e3e0d6d8b1b4e42c1038f
mickeymic25 <65719750+mickeymic25@users.noreply.github.com>
2026-02-01 21:41:07 +0000

    Update HeroBanner.jsx

--- src/components/sections/HeroBanner.jsx
+++ src/components/sections/HeroBanner.jsx
@@ -35,9 +35,9 @@ function HeroBanner() {
           Hi, my name is Micaela
         </motion.h1>
 
-        <motion.h4 variants={fadeInLeft} className="text-2xl md:text-4xl mb-3">
+        <motion.h6 variants={fadeInLeft} className="text-2xl md:text-4xl mb-3">
           Aspiring Front-end Developer
-        </motion.h4>
+        </motion.h6>
         
         
         <motion.p
